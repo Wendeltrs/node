60208f3e8499af92a5d54ad1e4b52d76fad4ef04
Wendel Tavares <wendeljt17@gmail.com>
2024-08-18 13:31:14 -0300

    creating seeds

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,11 @@ const startServer = () => {
 if(process.env.IS_LOCALHOST !== 'true'){
     Knex.migrate.latest()
         .then(() => {
-            startServer()
+            Knex.seed.run()
+                .then(() => {
+                    startServer()
+                })
+                .catch(console.log)
         })
         .catch(console.log)
 }else{
