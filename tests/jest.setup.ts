464187c0ced10fd94488a7f4e7480fc616baf00e
Wendel Tavares <wendeljt17@gmail.com>
2024-08-18 11:30:15 -0300

    updting some configs

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,4 +1,12 @@
 import supertest from "supertest"
 import { server } from "../src/server/server"
+import { Knex } from "../src/server/database/knex"
+
+beforeAll(async () => {
+    await Knex.migrate.latest()
+})
+afterAll(async () => {
+    await Knex.destroy()
+})
 
 export const testServer = supertest(server) //Retorna umas instância do servidor
